ca3095af700770c533d5ed639718802d4fb0a14a
Arpan Jha <62328473+arpanjha234@users.noreply.github.com>
2024-12-26 23:54:44 +0530

    updated missing-number.cpp
    minor change (logic remains same).

--- CSES/Introductory Problems/missing-number.cpp
+++ CSES/Introductory Problems/missing-number.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using i64 = long long;
  
 int main() {
   // fast-io
   ios::sync_with_stdio(0);
   cin.tie(0);
   
-  long long n, x = 0, sum1 = 0, sum2 = 0, res = 0;
+  i64 n, x = 0, sum1 = 0, sum2 = 0, res = 0;
   cin >> n;
  
   for(int i = 0; i < n; i++){
